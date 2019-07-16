aef1a942e54fe4b53cda497b37ccf529e6dd6bd6
Anastasiya Mazura <mazura.anastasiya@gmail.com>
2019-07-17 01:56:54 +0300

    Updates

---
+++ precache-manifest.e5ae3459f1b290933d155840fbbdfb3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac8b22049667c25fa5b072354c87ac83",
+    "url": "/ITGENIO/static/media/04.ac8b2204.png"
+  },
+  {
+    "revision": "80f383f6408d76f185f2",
+    "url": "/ITGENIO/static/js/runtime~main.80f383f6.js"
+  },
+  {
+    "revision": "4b46fc19348b7eb67a1c",
+    "url": "/ITGENIO/static/js/main.4b46fc19.chunk.js"
+  },
+  {
+    "revision": "5db2e18b2de6657c7a76",
+    "url": "/ITGENIO/static/js/1.5db2e18b.chunk.js"
+  },
+  {
+    "revision": "5db2e18b2de6657c7a76",
+    "url": "/ITGENIO/static/css/1.4b8984ac.chunk.css"
+  },
+  {
+    "revision": "fd155b605c6d5c8eefa4cd94ba7b54cc",
+    "url": "/ITGENIO/index.html"
+  }
+];
